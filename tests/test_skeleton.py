8cfd58ed4c14080a7c8fe8019087de9b1c2dcd38
Patrick Evans <ppevans11@gmail.com>
2020-08-09 18:04:20 +1200

    :sparkles: Added tests/

---
+++ tests/test_skeleton.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from putup_template.skeleton import fib
+
+__author__ = "Patrick Evans"
+__copyright__ = "Patrick Evans"
+__license__ = "MIT"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
