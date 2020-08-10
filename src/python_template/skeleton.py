d52f17a95728bc6022e375546b50b014e9505d5d
Patrick Evans <ppevans11@gmail.com>
2020-08-10 16:08:17 +1200

    :sparkles: Updated src/

--- src/python_template/skeleton.py
+++ src/python_template/skeleton.py
@@ -5,7 +5,7 @@ console script. To run this script uncomment the following lines in the
 [options.entry_points] section in setup.cfg:
 
     console_scripts =
-         fibonacci = putup_template.skeleton:run
+         fibonacci = python_template.skeleton:run
 
 Then run `python setup.py install` which will install the command `fibonacci`
 inside your current environment.
@@ -19,7 +19,7 @@ import argparse
 import sys
 import logging
 
-from putup_template import __version__
+from python_template import __version__
 
 __author__ = "Patrick Evans"
 __copyright__ = "Patrick Evans"
@@ -58,7 +58,7 @@ def parse_args(args):
     parser.add_argument(
         "--version",
         action="version",
-        version="putup-template {ver}".format(ver=__version__))
+        version="python-template {ver}".format(ver=__version__))
     parser.add_argument(
         dest="n",
         help="n-th Fibonacci number",
