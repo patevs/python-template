9ce27f04dab7b1c3e35ebb60b2cf6a5f426c2c54
Patrick Evans <ppevans11@gmail.com>
2022-06-22 22:02:13 +1200

    :page_facing_up: Update `copyright` in `docs/conf.py`

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'python-template'
-copyright = u'2020, Patrick Evans'
+copyright = u'2022, Patrick Evans'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
